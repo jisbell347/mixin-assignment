208b8ff1c9321e94a35dff39ee751e111a2f9c2a
Joseph Isbell <shadowwolf347@comcast.net>
2018-11-04 11:27:12 -0700

    Createed a global filter that returns the length of the text appended to the text.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('to-lowercase', function(value) {
-    return value.toLowerCase();
+Vue.filter('calculateLength', function(value) {
+    return value + ' (' + value.length + ')';
 });
 
 Vue.mixin({
